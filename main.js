b3c4450a17ff3b7ea4d31b9c8a77b9ad4f4cb18a
codesfromannywhere <codesfromannywhere@gmail.com>
2023-05-15 15:41:06 +0200

    ... Lev2

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 import { log } from "node:console";
 import { writeFile } from "node:fs/promises";
 
-async function getResponse() {
+
+// LEV I
+const getResponse = async () => {
+
     const response = await fetch(
         'https://jsonplaceholder.typicode.com/posts',
     );
 
     const json = await response.json();
-    console.log(json);
-    const newData = (JSON.stringify(json))
+    // console.log(json);
 
-    writeFile('./data/post.json', newData);
-    return "Yeah!";
-
+    writeFile('./data/post.json', JSON.stringify(json, null, 2));
+    return "Yeah - Die Daten sind da!";
 }
 
 console.log(getResponse());
@@ -20,18 +21,38 @@ console.log(getResponse());
 
 getResponse().then((resp) => console.log(resp));
 
-// function getResponse1() {
-//     return new Promise((resolve, reject) => {
-//         resolve();
-//     });
-// }
-// async function getResponse2() { }
 
-// console.log({ getResponse1: getResponse1(), getResponse2: getResponse2() });
 
-// try {
-//     writeFile('post.json', "");
-//     console.log("File has been saved.");
-// } catch (error) {
-//     console.error(error);
-// } 
+
+// LEV II
+
+const getComments = async () => {
+    let newCommentArr = [];
+    for (let index = 1; index <= 100; index++) {
+        const response = await fetch(
+            (`https://jsonplaceholder.typicode.com/posts/${index}/comments`),
+        );
+        const jsonComments = await response.json();
+        console.log(jsonComments);
+
+        // DECSTRUCTERING mit:
+        // push und Spread Operator [...]
+        // newCommentArr.push(...jsonComments)
+
+        // oder .concat
+        newCommentArr = newCommentArr.concat(jsonComments)
+    }
+
+    writeFile('./data/comments.json', JSON.stringify(newCommentArr, null, 2));
+
+    return "Yeah - Kommentare!";
+}
+
+console.log(getComments());
+
+getComments().then((resp) => console.log(resp));
+
+
+
+
+// LEV III
